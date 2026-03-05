9fc9c192353b4735654f6f70c2d9b0cc7accf5f2
felipenezmah28-hue <felipe.nezmah28@gmail.com>
2026-03-05 10:28:49 -0300

    comentário da alteração

---
+++ Basicpy.py
@@ -0,0 +1,12 @@
+from operator import truediv
+
+print("I can if I believe it")
+ #Entrada de Dados
+fruta=input("enter fruta")
+print(fruta)
+
+#Dados não strings
+codigo=int(input("enter codigo"))
+nome=input("enter nome")
+salario=float(input("enter salario"))
+print("Code: /n",codigo,"Name: ",nome,"Salary: ",salario)
--- Test.py
+++
@@ -1,1 +0,0 @@
-print( "Valeu Gu")
