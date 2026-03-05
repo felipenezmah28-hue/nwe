7c5bda4118beb84c3ccecc0aff31e81a9180d3fe
felipenezmah28-hue <felipe.nezmah28@gmail.com>
2026-03-05 12:37:33 -0300

    ok

--- Basicpy.py
+++ Basicpy.py
@@ -1,5 +1,3 @@
-from operator import truediv
-
 print("I can if I believe it")
  #Entrada de Dados
 fruta=input("enter fruta")
@@ -9,4 +7,14 @@ print(fruta)
 codigo=int(input("enter codigo"))
 nome=input("enter nome")
 salario=float(input("enter salario"))
-print("Code: /n",codigo,"Name: ",nome,"Salary: ",salario)
+print("Code: ",codigo,"Name: ",nome,"Salary: ",salario)
+
+
+#Operadores na pratica
+A=5
+B=15
+C=20
+print("A==B AND B>C", A==B and B>C)
+print("A<B OR B>C", A<B or B>C)
+print("NOT A==B: ", not A==B)
+print("NOT B>C: ", not B>C)
